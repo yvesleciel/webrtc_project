7fd9bf1d113ea9443c900bd5807c0919cfd2153c
yvesleciel <yvangoudjou04@gmail.com>
2023-04-04 14:35:08 +0200

    clean console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,7 @@ export class AppComponent {
 
   public constructor() {
     const firebaseConfig = {
-      apiKey: "AIzaSyBA70uNF3MAl8yFrju8_BVyaxnectKgY8E",
-      authDomain: "genius-d108d.firebaseapp.com",
-      databaseURL: "https://genius-d108d.firebaseio.com",
-      projectId: "genius-d108d",
-      storageBucket: "genius-d108d.appspot.com",
-      messagingSenderId: "758350542860",
-      appId: "1:758350542860:web:00904e0081f504738f5629"
+     // Insérer votre propre config ici
     };
 // Initialize Firebase
     initializeApp(firebaseConfig);
